54bcfe4d9339282a3e401198db9c08736e3cb4cf
LypkaB <lpk.bdn@gmail.com>
2021-01-25 19:55:26 +0200

    Refactor native JS to jQuery for «Header menu button»

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,9 +1,24 @@
-window.addEventListener('DOMContentLoaded', () => {
-    const adaptiveMenu = document.querySelector('.header__menu-btn'),
-          menu = document.querySelector('.header__inner');
+$(document).ready(function() {
+    /*<----- Header menu button ----->*/
+    $('.header__menu-btn').on('click', function() {
+        $(this).toggleClass('header__menu-btn_active');
+        $('.header__inner').toggleClass('header__inner_active');
+    });
 
-    adaptiveMenu.addEventListener('click', () => {
-        adaptiveMenu.classList.toggle('header__menu-btn_active');
-        menu.classList.toggle('header__inner_active');
-    })
-})
+    /*<----- Carousel ----->*/
+    $('.carousel__slider').slick({
+        speed: 1200,
+        adaptiveHeight: true,
+        prevArrow: '<button type="button" class="slick-prev"><img src="./assets/images/carousel/carousel__arrow-left.svg"></button>',
+        nextArrow: '<button type="button" class="slick-next"><img src="./assets/images/carousel/carousel__arrow-right.svg"></button>',
+        responsive: [
+            {
+                breakpoint: 768,
+                settings: {
+                    arrows: false,
+                    dots: true
+                }
+            }
+        ]
+    });
+});
