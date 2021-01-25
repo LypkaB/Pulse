4ef9373b2b5d1a472cfc603edbcfa6873e78f062
LypkaB <lpk.bdn@gmail.com>
2021-01-25 19:36:35 +0200

    Added adaptive for section «Carousel»

--- assets/js/carousel.js
+++ assets/js/carousel.js
@@ -3,6 +3,15 @@ $(document).ready(function() {
         speed: 1200,
         adaptiveHeight: true,
         prevArrow: '<button type="button" class="slick-prev"><img src="./assets/images/carousel/carousel__arrow-left.svg"></button>',
-        nextArrow: '<button type="button" class="slick-next"><img src="./assets/images/carousel/carousel__arrow-right.svg"></button>'
+        nextArrow: '<button type="button" class="slick-next"><img src="./assets/images/carousel/carousel__arrow-right.svg"></button>',
+        responsive: [
+            {
+                breakpoint: 768,
+                settings: {
+                    arrows: false,
+                    dots: true
+                }
+            }
+        ]
     });
 });
